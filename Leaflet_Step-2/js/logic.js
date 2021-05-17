934eff0c959e3494f19cce77dacfdc6a4e0f3c7d
Marisela Castillo <marisela2castillo@gmail.com>
2021-05-17 15:35:53 -0500

    Update logic.js

--- Leaflet_Step-2/js/logic.js
+++ Leaflet_Step-2/js/logic.js
@@ -1,18 +1,43 @@
-var myMap = L.map("map", {
-	center: [37.09, -95.71],
-	zoom: 5
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "satellite-v9",
+  accessToken: API_KEY
 });
+
+var grayscale = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+});
+
+var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "outdoors-v11",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+	"Satellite": satellite,
+	  "Grayscale": grayscale,
+	  "Outdoors": outdoors
+  };
   
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-	maxZoom: 18,
-	id: "light-v10",
-	accessToken: API_KEY
-}).addTo(myMap);
+  var layers = {
+	TECTONIC_LINE: new L.LayerGroup(),
+	EARTHQUAKES: new L.LayerGroup()
+  };
 
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-d3.json(queryUrl).then(function(data) {
+  var myMap = L.map("map", {
+	center: [23.6978, 120.9605],
+	zoom: 4,
+  layers: [
+		layers.TECTONIC_LINE,
+		layers.EARTHQUAKES
+	]
+});
 
 	function getColor(d) {
     return d >= 5 ? "rgb(240, 107, 107)" :
